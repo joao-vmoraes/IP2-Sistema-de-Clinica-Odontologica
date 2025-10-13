18b4c52000a018909ea46b85922b64484f5869e3
João Moraes <jvmmoraesdev@gmail.com>
2025-10-12 23:05:28 -0300

    Add getters and Setters in Pagamento

--- src/Pagamento.java
+++ src/Pagamento.java
@@ -15,6 +15,38 @@ public class Pagamento {
         this.referenciaProcedimento = referenciaProcedimento;
     }
 
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
+    }
+
+    public void setConfirmado(boolean confirmado) {
+        this.confirmado = confirmado;
+    }
+
+    public LocalDateTime getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDateTime dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public Procedimento getReferenciaProcedimento() {
+        return referenciaProcedimento;
+    }
+
+    public void setReferenciaProcedimento(Procedimento referenciaProcedimento) {
+        this.referenciaProcedimento = referenciaProcedimento;
+    }
+
     //MÉTODOS
     public boolean isConfirmado() {
         return confirmado;
